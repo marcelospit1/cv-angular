84175e00941f0fc0a4cc1c016abf1655fb8974d7
Marcelo Siles <marcepsit27@gmail.com>
2025-09-16 19:29:55 -0400

    cambio de estilos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
+
 @Component({
   selector: 'app-root',
   standalone: true,
   templateUrl: './app.html',
-  styleUrls: ['./app.scss']   // ✅ en plural y con array
+  styleUrls: ['./app.scss']
 })
-
 export default class App {   
   year = new Date().getFullYear();
 }
